f5bce6f5e6758d71a6d96ccf6b0ebf05b5dd9096
Anchidin Damian <damisv@gmail.com>
2018-04-27 19:47:12 +0300

    CalendarEvent class fix

--- src/client/app/models/calendarEvent.ts
+++ src/client/app/models/calendarEvent.ts
@@ -5,10 +5,10 @@ export class MyCalendarEvent implements CalendarEvent {
   constructor(public start: Date = new Date(),
               public end: Date = new Date(),
               public title: string = 'default',
-              public draggable: boolean = true,
               public color: any = colors.red,
-              public meta: any = null,
               public actions: any[] = [],
+              public meta: any = null,
+              public draggable: boolean = true,
               public _id?: string) {}
 }
 
